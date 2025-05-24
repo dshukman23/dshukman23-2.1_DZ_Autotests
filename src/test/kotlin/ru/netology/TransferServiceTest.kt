1cde9bd28526ff23be812184be7290494c355af8
Dmitry Shukman <dmshukman@gmail.com>
2025-05-24 17:28:17 +0300

    Updating the autotest

--- src/test/kotlin/ru/netology/TransferServiceTest.kt
+++ src/test/kotlin/ru/netology/TransferServiceTest.kt
@@ -6,6 +6,36 @@ class TransferServiceTest {
     @org.junit.Test
     fun calculateCommission() {
         val service = TransferService() // создаем экземпляр
+
+        assertThrows(IllegalArgumentException::class.java) {
+            service.calculateCommission(CardType.VISA, 0, 150001)
+        }
+
+        assertThrows(IllegalArgumentException::class.java) {
+            service.calculateCommission(CardType.MASTERCARD, 0, 600001)
+        }
+
+        assertThrows(IllegalArgumentException::class.java) {
+            service.calculateCommission(CardType.VK_PAY, 0, 15001)
+        }
+
+        assertThrows(IllegalArgumentException::class.java) {
+            service.calculateCommission(CardType.VK_PAY, 0, 40001)
+        }
+
+
+        // Для Mastercard без комиссии
+        assertEquals(0, service.calculateCommission(CardType.MASTERCARD, 0, 50000))
+
+        // Для Visa с минимальной комиссией
+        assertEquals(35, service.calculateCommission(CardType.VISA, 0, 4667))
+
+        // Для Mastercard с комиссией
+        assertEquals(620, service.calculateCommission(CardType.MASTERCARD, 0, 100000))
+
+        // Для VK Pay всегда 0
+        assertEquals(0, service.calculateCommission(CardType.VK_PAY, 0, 10000))
+
         val cardType = CardType.VISA
         val monthlyAmount = 0
         val transferAmount = 100_000
